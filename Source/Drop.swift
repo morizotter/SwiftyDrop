ff769408f2ad69602f32c9a58ad6f2888044d888
Naoki Morita <namorit@gmail.com>
2015-06-18 03:11:50 +0900

    Show Drop

--- Source/Drop.swift
+++ Source/Drop.swift
@@ -10,4 +10,56 @@ import UIKit
 
 class Drop: UIView {
     
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+}
+
+extension Drop {
+    class func down(title: String, subtitle: String?) {
+        let drop = UINib(nibName: "Drop", bundle: nil).instantiateWithOwner(nil, options: nil)[0] as! Drop
+        
+        if let window = window() {
+            window.addSubview(drop)
+            
+            let height = CGFloat(100.0)
+            
+            let heightConstraint = NSLayoutConstraint(
+                item: drop,
+                attribute: .Height,
+                relatedBy: .Equal,
+                toItem: nil,
+                attribute: .Height,
+                multiplier: 1.0,
+                constant: height
+            )
+            
+            let constraints = ([.Top, .Left, .Right] as [NSLayoutAttribute]).map {
+                return NSLayoutConstraint(
+                    item: window,
+                    attribute: $0,
+                    relatedBy: .Equal,
+                    toItem: drop,
+                    attribute: $0,
+                    multiplier: 1.0,
+                    constant: 0.0
+                )
+            }
+            
+            drop.setTranslatesAutoresizingMaskIntoConstraints(false)
+            drop.addConstraint(heightConstraint)
+            window.addConstraints(constraints)
+            drop.layoutIfNeeded()
+        }
+        
+    }
+    
+    class func up() {
+        
+    }
+}
+
+extension Drop {
+    private class func window() -> UIWindow? {
+        return UIApplication.sharedApplication().keyWindow
+    }
 }
--- SwiftyDrop/SwiftyDrop/ViewController.swift
+++ SwiftyDrop/SwiftyDrop/ViewController.swift
@@ -15,11 +15,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func dropdown(sender: AnyObject) {
+        Drop.down("Title", subtitle: "subtitle")
     }
-
-
 }
 
