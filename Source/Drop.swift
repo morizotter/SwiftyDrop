9ba60c6335d8125cd5fc8e12ec757a57edd50879
Naoki Morita <namorit@gmail.com>
2015-06-18 01:45:29 +0900

    Restructure

---
+++ Source/Drop.swift
@@ -0,0 +1,13 @@
+//
+//  Drop.swift
+//  SwiftyDrop
+//
+//  Created by MORITANAOKI on 2015/06/18.
+//  Copyright (c) 2015年 MORITANAOKI. All rights reserved.
+//
+
+import UIKit
+
+class Drop: UIView {
+    
+}
