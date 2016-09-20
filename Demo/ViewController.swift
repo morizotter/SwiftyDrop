139c6bdabab3dbd46769d5ea4af550ba9dccdc97
Naoki Morita <namorit@gmail.com>
2016-09-21 01:50:44 +0900

    Update range syntax

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -129,7 +129,8 @@ class ViewController: UIViewController {
     func sampleText() -> String {
         let text = "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda."
         let length = Int(arc4random_uniform(100)) + 10
+        let start = text.startIndex
         let end = text.startIndex.advancedBy(length)
-        return text.substringWithRange(Range(start: text.startIndex, end: end))
+        return text.substringWithRange(start..<end)
     }
 }
