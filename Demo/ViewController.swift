29b009a2d4c111b37345729d830111c1abe8a2dc
Naoki Morita <namorit@gmail.com>
2017-09-25 22:51:50 +0900

    Add additional Swift4 changes

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -118,7 +118,7 @@ class ViewController: UIViewController {
         present(controller, animated: true, completion: nil)
     }
     
-    func upAllDrops(_ sender: AnyObject) {
+    @objc func upAllDrops(_ sender: AnyObject) {
         if let hidden = navigationController?.isNavigationBarHidden {
             navigationController?.setNavigationBarHidden(!hidden, animated: true)
         }
@@ -130,6 +130,6 @@ class ViewController: UIViewController {
         let text = "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda."
         let length = Int(arc4random_uniform(100)) + 10
         let end = text.characters.index(text.startIndex, offsetBy: length)
-        return text.substring(with: (text.startIndex ..< end))
+        return String(text[text.startIndex ..< end])
     }
 }
